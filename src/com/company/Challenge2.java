db10e87be538a3b26b4ab666ee25e9f57473b61a
pittss23 <pittss23@greenhill.org>
2020-06-08 10:16:03 -0500

    This is an update.

--- src/com/company/Challenge2.java
+++ src/com/company/Challenge2.java
@@ -15,26 +15,26 @@ public static final int SIZE=5;
 
     public static void Line() {
         System.out.print("+");
-        for (int dash = 0; dash < SIZE; dash++) {
+        for (int dash = 0; dash < 9; dash++) {
             System.out.print("-");
         }
         System.out.println("+");
     }
 
     public static void Triangle() {
-        for (int line = 1; line < SIZE; line++) {
+        for (int line = 1; line < 5; line++) {
             System.out.print("|");
-            for (int space = 1; space <= SIZE - line; space++) {
+            for (int space = 1; space <= 5 - line; space++) {
                 System.out.print(" ");
             }
-            for (int slash = 1; slash <= SIZE-line; slash++) {
+            for (int slash = 1; slash <= -1 + line; slash++) {
                 System.out.print("/");
             }
             System.out.print("*");
-            for (int slash = 1; slash <= SIZE-line; slash++) {
+            for (int slash = 1; slash <= -1 + line; slash++) {
                 System.out.print("\\");
             }
-            for (int space = 1; space <= SIZE - line; space++) {
+            for (int space = 1; space <= 5 - line; space++) {
                 System.out.print(" ");
             }
             System.out.println("|");
@@ -43,7 +43,7 @@ public static final int SIZE=5;
     }
 
     public static void UpsideDownTriangle() {
-        for (int line = 1; line < SIZE; line++) {
+        for (int line = 1; line < 5; line++) {
             System.out.print("|");
             for (int space = 1; space <=line; space++) {
                 System.out.print(" ");
