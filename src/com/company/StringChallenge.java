81f10841ce1a32bbf26c8520ff895ba5b0051f6b
pittss23 <pittss23@greenhill.org>
2020-06-21 23:58:10 -0500

    update

--- src/com/company/StringChallenge.java
+++ src/com/company/StringChallenge.java
@@ -1,39 +1,50 @@
 package com.company;
+//Sydney Pitts
+//6/21/20
+
+//import and label scanner
+//use scanner to ask the user for an integer
+//label this integer
+//create an if statement that will continue the program because if the # is 0 or below the program can't continue
+//generate a loop withing the if statement so that the method you are about to create will run the # of times of the int
+//create a method and repeat the steps to make a scanner (with a different name because you can't use the same scanner)
+//ask the user for a word
+//make 2 loops (one for even and one for odd)
+//make a return statement for the even and odds
 
 import java.util.Scanner;
 public class StringChallenge {
-
-    private static String ScanPrint;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter an integer:");
+        System.out.print("Please enter an integer:");//initial code and I don't need to loop it
         int i = input.nextInt();
-        input.close();
+        String a = "";//makes return a string
         if (i >= 1) {
-            for (int line = 1; line <= i; line++) {
-                System.out.println(ScanPrint);
-            }
+            for (int line = 1; line <= i; line++) {//makes phrase run the amount of times given
+            a = Phrase();
+            System.out.println(a);
         }
     }
+}
 
-    public static void ScanPrint() {
+    public static String Phrase() {
         int lS;
         Scanner keyboard = new Scanner(System.in);
-        System.out.print("Please enter your word:");
+        System.out.print("Please enter your word:");//
         String name = keyboard.nextLine();
-        int indexOf = name.indexOf(" ");
-        String S = name.substring(0, indexOf);
-        lS = S.length();
-        for (int line = 0; line <= lS; line += 2) {
-            if (line % 2 == 0) {
-                System.out.print(S.charAt(line));
+        String out = "";
+        lS = name.length();
+        for (int even = 0; even < lS; even += 2) {
+            if (even % 2 == 0) {
+                out += name.charAt(even);
             }
         }
-        System.out.print(" ");
-        for (int line = 1; line <= lS; line += 2) {
-            if (line % 2 == 1) {
-                System.out.print(S.charAt(line));
+        out = out + " ";//makes return a string
+        for (int odd = 1; odd < lS; odd += 2) {
+            if (odd % 2 == 1) {
+                out += name.charAt(odd);
             }
         }
+        return out;
     }
 }
